4e7d363a66bc2cbd17760511d0ecf5d9759dcf55
“Daniel <danieljuarez1996@gmail.com>
2023-06-01 18:02:07 -0300

    creacion del archivo principal y archivo .gitign

---
+++ practico7/Calculadora.cs
@@ -0,0 +1,16 @@
+namespace EspacioCalculadora;
+
+public  class Calculadora()
+{
+    
+
+
+public void Sumar(double termino)
+public void Restar(double termino)
+public void Multiplicar(double termino)
+public void Dividir(double termino)
+public void Limpiar()
+
+
+
+}
