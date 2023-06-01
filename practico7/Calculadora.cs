ca3075121d4cb86e00ca7b05a3b939cf2ec219e3
“Daniel <danieljuarez1996@gmail.com>
2023-06-01 19:41:25 -0300

    correcciones generalesdel punto 1 del practico

--- practico7/Calculadora.cs
+++ practico7/Calculadora.cs
@@ -1,16 +1,60 @@
+// See https://aka.ms/new-console-template for more information
 namespace EspacioCalculadora;
 
-public  class Calculadora()
+
+public class Calculadora
 {
-    
 
 
-public void Sumar(double termino)
-public void Restar(double termino)
-public void Multiplicar(double termino)
-public void Dividir(double termino)
-public void Limpiar()
+    private double dato = 0;
+    private double resultado;
+    public double Dato { get => dato; set => dato = value; }
+    public double Resultado { get => Dato; }
 
+
+    public void Sumar(double termino)
+    {
+        Dato = Dato + termino;
+       
+    }
+    public void Restar(double termino)
+    {
+        Dato = Dato - termino;
+
+    }
+    public void Multiplicar(double termino)
+    {
+        Dato = Dato * termino;
+
+    }
+    public void Dividir(double termino)
+    {
+        if (termino != 0)
+        {
+
+            Dato = Dato / termino;
+        }
+        else
+        {
+            Console.WriteLine("No se puede realizar la division ");
+        }
+
+    }
+    public void Limpiar()
+    {
+        //Vuelve el valor a 0
+        Dato = 0;
+        Console.WriteLine("Resultado volvio a su valor original: " + Dato);
+    }
+
+    public void Mostrar()
+    {
+        Console.WriteLine("El Resultado es: " + Resultado);
+    }
 
 
 }
+
+
+
+
--- practico7/Program.cs
+++ practico7/Program.cs
@@ -2,11 +2,120 @@
 using EspacioCalculadora;
 Console.WriteLine("Hello, World!");
 
+double termino = 0;
+bool funciona1 = false;
+string? numString1 = "";
+int opcion;
+
+Calculadora Ndato = new Calculadora();
+
+do
+{
+    Console.WriteLine("Elija una opcion de la calculadora:");
+    Console.WriteLine("1)Sumar\n 2)Restar\n 3)Multiplicar\n 4)Dividir\n 5)Limpiar\n 6)Salir\n");
+    opcion = Console.ReadKey().KeyChar;
 
 
+    switch (opcion)
+    {
+        case '1':
+            do
+            {
+                Console.WriteLine("\nIngrese los números en la calculadora: (Para salir al menú principal ingrese cualquier letra)");
+                numString1 = Console.ReadLine();
+                funciona1 = double.TryParse(numString1, out termino);
+                if (!funciona1)
+                {
+                    Console.WriteLine(numString1 + " No es un numero valido");
+                }
+                else
+                {
+                    {
+                        Ndato.Sumar(termino);
+                        Console.WriteLine("El Resultado es: " + Ndato.Resultado);
+
+                    }
+                }
+
+            } while (funciona1);
+            break;
+        case '2':
+            do
+            {
+                Console.WriteLine("\nIngrese los números en la calculadora: (Para salir al menú principal ingrese cualquier letra)");
+                numString1 = Console.ReadLine();
+                funciona1 = double.TryParse(numString1, out termino);
+                if (!funciona1)
+                {
+                    Console.WriteLine(numString1 + " No es un numero valido");
+                }
+                else
+                {
+                    {
+                        Ndato.Restar(termino);
+                        Console.WriteLine("El Resultado es: " + Ndato.Resultado);
+
+                    }
+                }
+
+            } while (funciona1);
+            break;
+        case '3':
+            do
+            {
+                Console.WriteLine("\nIngrese los números en la calculadora: (Para salir al menú principal ingrese cualquier letra)");
+                numString1 = Console.ReadLine();
+                funciona1 = double.TryParse(numString1, out termino);
+                if (!funciona1)
+                {
+                    Console.WriteLine(numString1 + " No es un numero valido");
+                }
+                else
+                {
+                    {
+                        Ndato.Multiplicar(termino);
+                        Console.WriteLine("El Resultado es: " + Ndato.Resultado);
+
+                    }
+                }
+
+            } while (funciona1);
+            break;
+        case '4':
+            do
+            {
+                Console.WriteLine("\nIngrese los números en la calculadora: (Para salir al menú principal ingrese cualquier letra)");
+                numString1 = Console.ReadLine();
+                funciona1 = double.TryParse(numString1, out termino);
+                if (!funciona1)
+                {
+                    Console.WriteLine(numString1 + " No es un numero valido");
+                }
+                else
+                {
+                    {
+                        Ndato.Dividir(termino);
+                        Console.WriteLine("El Resultado es: " + Ndato.Resultado);
+
+                    }
+                }
+
+            } while (funciona1);
+            break;
+        case '5':
+            Ndato.Limpiar();
+            break;
+        case '6':
+            Console.WriteLine("Saliendo del programa");
+
+            break;
 
 
+        default:
 
+            break;
+    }
+} while (opcion != 6);
 
 
 
